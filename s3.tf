9e1b8622c3698f498584593d6e88e711e1cef3ba
filola <hsoorang@naver.com>
2023-02-07 17:23:13 +0900

    [Update]acm certificatie resource

--- s3.tf
+++ s3.tf
@@ -2,6 +2,9 @@ resource "aws_s3_bucket" "test" {
   bucket = "terraform-inflearn-chs"
 }
 
-resource "aws_s3_bucket" "swit" {
-  bucket = "swit-test-0808"
+resource "aws_acm_certificate" "swit" {
+  provider = "aws.virginia"
+
+  domain_name = "swit.backend.co"
+  validation_method = "EMAIL"
 }
