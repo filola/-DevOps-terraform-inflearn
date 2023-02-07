b9334073c767640dce3f416f66285dc319261263
filola <hsoorang@naver.com>
2023-02-07 17:13:29 +0900

    [Update]us-east-1 region alias

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
   region = "ap-northeast-2"
 }
+
+// cloudfront의 경우 인증서가 버지니아 북부에 밖에 생성이 안되므로
+provider "aws" {
+  alias = "virginia"
+  region = "us-east-1"
+}
--- s3.tf
+++ s3.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "test" {
   bucket = "terraform-inflearn-chs"
 }
+
+resource "aws_s3_bucket" "swit" {
+  bucket = "swit-test-0808"
+}
