0b7865473581e6028288c8b328d6a194d645474d
filola <hsoorang@naver.com>
2023-02-21 09:49:46 +0900

    [Add] VPC subnet

--- s3.tf
+++ s3.tf
@@ -2,9 +2,9 @@ resource "aws_s3_bucket" "test" {
   bucket = "terraform-inflearn-chs"
 }
 
-resource "aws_acm_certificate" "swit" {
-  provider = "aws.virginia"
+# resource "aws_acm_certificate" "swit" {
+#   provider = "aws.virginia"
 
-  domain_name = "swit.backend.co"
-  validation_method = "EMAIL"
-}
+#   domain_name = "swit.backend.co"
+#   validation_method = "EMAIL"
+# }
--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,47 @@ resource "aws_vpc" "terraform-chs" {
     Name = "terraform-chs"
   }
 }
+
+resource "aws_subnet" "public_subnet1" {
+  vpc_id = aws_vpc.terraform-chs.id
+  cidr_block = "10.0.0.0/24"
+
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "terraform-chs-public-subnet1"
+  }
+}
+
+resource "aws_subnet" "public_subnet2" {
+  vpc_id = aws_vpc.terraform-chs.id
+  cidr_block = "10.0.16.0/24"
+
+  availability_zone = "ap-northeast-2b"
+  
+  tags = {
+    Name = "terraform-chs-public-subnet2"
+  }
+}
+
+resource "aws_subnet" "private_subnet1" {
+  vpc_id = aws_vpc.terraform-chs.id
+  cidr_block = "10.0.128.0/24"  
+
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "terraform-chs-private-subnet1"
+  }
+}
+
+resource "aws_subnet" "private_subnet2" {
+  vpc_id = aws_vpc.terraform-chs.id
+  cidr_block = "10.0.144.0/24"
+
+  availability_zone = "ap-northeast-2b"
+
+  tags = {
+    Name = "terraform-chs-private-subnet2"
+  }
+}
